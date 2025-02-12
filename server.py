a7060fabc58c47d9559d5b851735e6189a631034
carc-droid <camilla.bachinger@gmail.com>
2025-02-12 16:48:49 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from EmotionDetection.emotion_detection import emotion_detector  # Importing the emotion detector function
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
@@ -8,7 +8,7 @@ app = Flask("Emotion Detection")
 def sent_detector():
     # Get the text input from the form or JS
     text_to_analyze = request.args.get('textToAnalyze')
-
+    
     # Pass text and store response
     response = emotion_detector(text_to_analyze)
 
@@ -21,9 +21,10 @@ def sent_detector():
             f"The dominant emotion is {response['dominant_emotion']}."
         )
 
-
-    if not text_to_analyze:
-        return "Invalid Text! Please try again.", 400
+    # Check if the dominant emotion is None (invalid text input or API error)
+    if response['dominant_emotion'] is None:
+        # If dominant_emotion is None, return an error message in the same format
+        formatted_emotions = "Invalid text! Please try again."
 
     # Return the formatted string
     return formatted_emotions, 200
@@ -32,5 +33,5 @@ def sent_detector():
 def render_index_page():
     return render_template('index.html')
 
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4012)
